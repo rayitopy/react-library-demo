fc6fa763ac09d5e6a003f0e3509df09b1b184e0a
Daniel Martín <danielme10114c@gmail.com>
2017-09-26 18:26:43 +0100

    Add project to github

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Javascript: The good parts', pages: 101 },
+        {title: 'Harry potter', pages: 39},
+        {title: 'The dark tower', pages: 185},
+        {title: 'Eloquent Ruby', pages: 17}
+    ]
+}
